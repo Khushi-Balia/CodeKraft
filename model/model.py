783bb9dc8d1a3ef86bbde738f2e0fbfa99310a1a
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-17 19:01:03 +0530

    Starting backend work

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 from IPython.display import Markdown
 import os
 
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = "AIzaSyCGOYJNkoztEVxDN28qgzhe1VCb-RGSh6c"
 
 model = genai.GenerativeModel('gemini-pro')
 
@@ -11,4 +11,4 @@ genai.configure(api_key = GEMINI_API_KEY)
 prompt = "I have two databases Student where the fields are Student_id, Student_name and Grades where the fields are Student_id, Marks. Give me SQL Query to output name of all students with grade greater than 80."
 
 response = model.generate_content(prompt)
-Markdown(response.text)
+print(response.text)
